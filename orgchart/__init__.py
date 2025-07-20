835826428308b9a40943f5b7c3936a1a363e213c
weebmaster2405 <aniketbankar3077@gmail.com>
2025-07-21 02:24:15 +0530

    Update __init__.py

--- orgchart/__init__.py
+++ orgchart/__init__.py
@@ -1,3 +1,6 @@
-import pymysql
-
-pymysql.install_as_MySQLdb()
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    # pymysql not available, using default database backend
+    pass
